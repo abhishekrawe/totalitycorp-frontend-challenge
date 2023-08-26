b0c0d745f55ae7ed1569aaacc9645c791b575e72
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-08-26 17:31:45 +0530

    added styling to home and product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Home from "./component/Home";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello </h1>
+      <>
+        <Home />
+      </>
     </div>
   );
 }
