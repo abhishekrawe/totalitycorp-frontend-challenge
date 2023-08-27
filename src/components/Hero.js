62f7663f24c1952513bfa4ca6fb42c93a7c311ac
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-08-27 11:22:23 +0530

    Created Home and Hero Functionality add styling

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+        <div class="keyboard">
+          <span class="key">T</span>
+          <span class="key">O</span>
+          <span class="key">T</span>
+          <span class="key">A</span>
+          <span class="key">L</span>
+          <span class="key">I</span>
+          <span class="key">T</span>
+          <span class="key">Y</span>
+        </div>
+      </div>
+  );
+}
+
+export default Hero;
