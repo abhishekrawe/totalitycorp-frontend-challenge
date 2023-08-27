1db2b28b5250ae7f8ccc7ad5d1ace5c5cbb1256e
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-08-27 11:25:29 +0530

    imported bootstrap and Context in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import Context from "./context/Context";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <Context>
+      <App />
+    </Context>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
